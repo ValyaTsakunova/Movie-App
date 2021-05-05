2fc1905e59e77fce72e24b6707623ce3f1d603ba
Valya Tsakunova <vtsakunova@mail.ru>
2021-05-05 16:17:30 +0300

    add redux

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import './SearchForm.css';
 
 function SearchForm(){
+    const [inputValue, setInputValue] = useState('');
     const onSubmit = (ev) => {
         ev.preventDefault();
+        
+        setInputValue('');
+    }
+    const onInputChange = (ev) => {
+        setInputValue(ev.target.value);
     }
   return (
     <>
       <form onSubmit={onSubmit}  className="form">
-        <input placeholder="Type here.." className="searchInput" />
+        <input value={inputValue} onChange={onInputChange} placeholder="Type here.." className="searchInput" />
         <Button variant="success" type="submit">Search</Button>
       </form>
     </>
---
+++ src/state-management/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    movies: []
+}
+
+export function reducer(state = initialState, action){
+    switch(action.type){
+
+        default:
+            return state
+    }
+}
