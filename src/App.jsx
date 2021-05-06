c84a711d143737162b3345d279cd1efc8141e441
Valya Tsakunova <vtsakunova@mail.ru>
2021-05-06 20:15:28 +0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {  useSelector } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 import SearchForm from './components/SearchForm';
 import Movies from './components/Movies';
+import NotFound from './components/NotFound'
 import './App.css';
 
 function App() {
@@ -9,8 +11,16 @@ function App() {
   return (
     <div className="wrapper">
     <div className="title"><p>Movie App</p></div>
-      <SearchForm />
-      {movies.length > 0 ? <Movies movies={movies}/> : ''}
+    <Switch>
+      <Route path="/" exact>
+          <SearchForm />
+          {movies.length > 0 ? <Movies movies={movies}/> : ''}
+      </Route>
+      <Route >
+          <NotFound/>
+      </Route>
+    </Switch>
+      
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import store from './state-management';
@@ -9,8 +10,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
