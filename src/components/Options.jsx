ca16e43d2f1cf62cabe602fd843efe8b9cb0e9c0
Valya Tsakunova <vtsakunova@mail.ru>
2021-05-12 00:07:03 +0300

    fix buttons

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ButtonGroup, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSortAmountDownAlt } from '@fortawesome/free-solid-svg-icons';
@@ -11,36 +11,48 @@ const sortDownAlt = <FontAwesomeIcon icon={ faSortAmountDownAlt } />;
 const sortDown = <FontAwesomeIcon icon={ faSortAmountDown } />;
 
 function Sorting() {
+    const [ releaseButton, setReleaseButton] = useState(false);
+    const [ ratingButton, setRatingButton] = useState(false);
+    const [ sortAltButton, setSortAltButton] = useState(false);
+    const [ sortDownButton, setSortDownButton] = useState(false);
+
     const onReleaseClick = () => {
         store.dispatch({type: 'SORT_BY_RELEASE_DATE'} );
-        // debugger
+        setReleaseButton(true);
+        setRatingButton(false)
       }
   
-      const onRatingClick = () => {
-        store.dispatch({type: 'SORT_BY_VOTE_AVERAGE'} );
-      }
+    const onRatingClick = () => {
+      store.dispatch({type: 'SORT_BY_VOTE_AVERAGE'} );
+      setRatingButton(true);
+      setReleaseButton(false);
+    }
 
       const onSortDownAlt = () => {
         store.dispatch({type: 'SORT_ORDER_DOWN_ALT'} );
+        setSortAltButton(true);
+        setSortDownButton(false)
       }
 
       const onSortDown = () => {
         store.dispatch({type: 'SORT_ORDER_DOWN'} );
+        setSortDownButton(true);
+        setSortAltButton(false);
       }
 
     return(
         <div className="options">
         <div>Options for your sort</div>
         <div>Sort by</div>
-         <ButtonGroup aria-label="Basic example">
-                <Button variant="secondary" onClick={onReleaseClick} className="buttonRelease" >Release</Button>
-                <Button variant="secondary" onClick={onRatingClick} className="buttonRating">Rating</Button>
-        </ButtonGroup>
+         <div>
+                <button onClick={onReleaseClick} className={releaseButton ? "active" : null} >Release</button>
+                <button onClick={onRatingClick} className={ratingButton ? "active" : null}>Rating</button>
+        </div>
         <div>Order</div>
-         <ButtonGroup aria-label="Basic example">
-                <Button variant="secondary" onClick={onSortDownAlt} className="buttonRelease" >{sortDownAlt} </Button>
-                <Button variant="secondary" onClick={onSortDown} className="buttonRating">{sortDown}</Button>
-        </ButtonGroup>
+         <div>
+                <button onClick={onSortDownAlt} className={sortAltButton ? "active" : null} >{sortDownAlt} </button>
+                <button onClick={onSortDown} className={sortDownButton ? "active" : null}>{sortDown}</button>
+        </div>
         </div>
     )
 }
